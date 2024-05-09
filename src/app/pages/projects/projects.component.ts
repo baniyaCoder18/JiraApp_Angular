761a9998ffb6f314742e90c759f1a4b2e17f4bb0
baniyaCoder18 <rahul.sgagrawal@gmail.com>
2024-05-09 16:11:13 +0800

    first commit

---
+++ src/app/pages/projects/projects.component.ts
@@ -0,0 +1,52 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.css']
+})
+export class ProjectsComponent implements OnInit{
+
+  projectList: any [] = [];
+  projectObj: any = {
+    "projectId": 0,
+    "projectName": "",
+    "shortName": "",
+    "createdDate": new Date()
+  }
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.getAllProjects();
+  }
+
+  getAllProjects() {
+    this.http.get("https://freeapi.miniprojectideas.com/api/Jira/GetAllProjects").subscribe(
+      (res: any) => {
+        this.projectList = res.data;
+      },
+      (error: any) => {
+        console.error("An error occurred while fetching project data:", error);
+        this.projectList = [
+          { projectName: 'Project 1' },
+          { projectName: 'Project 2' }
+        ];
+      }
+    );
+
+  }
+  onSave() {
+    this.http.post("https://freeapi.miniprojectideas.com/api/Jira/CreateProject",this.projectObj).subscribe((res: any)=>{
+       if(res.result) {
+        alert(res.message);
+        this.getAllProjects();
+       } else {
+        alert(res.message)
+       }
+    })
+  }
+}
